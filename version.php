b8d4f5641f4e592bdb8d77cef19c4d6e52e82f5e
Céline Pervès <cperves@unistra.fr>
2024-03-25 16:05:14 +0100

    unique version number

--- version.php
+++ version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_molstructure';
 $plugin->release = '2.0.0';
-$plugin->version = 2024032500;
+$plugin->version = 2024032502;
 $plugin->requires = 2023100903;
 $plugin->maturity   = MATURITY_STABLE;
