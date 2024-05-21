26fcc7f20d609ff6db151c701eeae605a23af75e
Céline Pervès <cperves@unistra.fr>
2024-05-21 09:45:01 +0200

    eslint disable add for promise

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_molstructure';
-$plugin->release = '1.0.3';
-$plugin->version = 2024032501;
+$plugin->release = '1.0.4';
+$plugin->version = 2024052100;
 $plugin->requires = 2020110911;
 $plugin->maturity = MATURITY_STABLE;
