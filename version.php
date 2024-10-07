2aabaea25e9475cf9a9adad94a4bfa3e78535919
Céline Pervès <cperves@unistra.fr>
2024-10-07 11:59:43 +0200

    [#7] new version thxs @tailetan

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_molstructure';
-$plugin->release = '2.0.1';
-$plugin->version = 2024060300;
+$plugin->release = '2.0.2';
+$plugin->version = 2024100700;
 $plugin->requires = 2023042407;
 $plugin->maturity   = MATURITY_STABLE;
