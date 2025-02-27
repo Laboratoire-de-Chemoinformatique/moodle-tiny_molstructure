d7bf80087baaa85e261cfe5a8d20ebbc6bc1b4e5
Céline Pervès <cperves@unistra.fr>
2025-02-27 15:04:47 +0100

    change version and release number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 # Change log
+* 27/02/2025 new version Moodle 4.5 version with various views : 2D, 3D, Spectrum 
 * 19/03/2024 tiny version of previous atto molstructure plugin
--- version.php
+++ version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_molstructure';
-$plugin->release = '1.1.0';
-$plugin->version = 2025022100;
+$plugin->release = '2.1.0';
+$plugin->version = 2025022700;
 $plugin->requires  = 2024100701;
